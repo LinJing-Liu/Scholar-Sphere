f7c876b3357ded370174096b5c0fea0719392ad8
Harim Hahn <hmh79@cornell.edu>
2023-06-19 16:54:08 -0400

    words stay after refresh

--- website-backend/app.py
+++ website-backend/app.py
@@ -15,37 +15,6 @@ def home():
     return "my API"
 
 
-# async def handle_message(websocket, path):
-#     while True:
-#         message = await websocket.recv()
-#         data = json.loads(message)
-
-#         # Extract the word data from the received message
-#         input_word = data.get("word", "")
-#         input_definition = data.get("definition", "")
-#         input_explanation = data.get("explanation", "")
-#         input_example = data.get("example", "")
-#         input_tag = data.get("tag", "")
-
-#         # Store the word data in a dictionary
-#         word_data = {
-#             "word": input_word,
-#             "definition": input_definition,
-#             "explanation": input_explanation,
-#             "example": input_example,
-#             "tag": input_tag,
-#         }
-#         # Send the updated word data to all connected clients
-#         for client in websockets:
-#             await client.send(json.dumps(word_data))
-
-
-# start_server = websockets.serve(handle_message, "localhost", 8765)
-
-# asyncio.get_event_loop().run_until_complete(start_server)
-# asyncio.get_event_loop().run_forever()
-
-
 @app.route("/api/addterm", methods=["POST"])
 def receive_entry():
     data = request.get_json()
--- website-react/src/App.js
+++ website-react/src/App.js
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import io from 'socket.io-client';
+import ReactCardFlip from 'react-card-flip';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <h1>
           ScholarSphere
-        </p>
+        </h1>
 
       </header>
       <body className="App-body">
-        <p>
-          This is my body
-        </p>
         <WordList></WordList>
+        <Game></Game>
       </body>
     </div >
   );
@@ -32,9 +31,9 @@ function WordList() {
 
   return (
     <div>
-      <h2>My word list: </h2>
-
+      <h1>My word list: </h1>
       <Word data={word_example} ></Word>
+
       <InputListener></InputListener>
     </div>
   );
@@ -42,7 +41,7 @@ function WordList() {
 
 function Word({ data }) {
   return (
-    <div>
+    <div className='flashcard'>
       <h2>{data.word}</h2>
       <p>Definition: {data.definition}</p>
       <p>Explanation: {data.explanation}</p>
@@ -51,21 +50,60 @@ function Word({ data }) {
     </div>
   );
 }
+
+//!!!! doesn't work
+function FlashCard({ data }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <div onClick={handleClick} className='flashcard'>
+        {/* Front of the card */}
+        <h2>{data.word}</h2>
+      </div>
+
+      <div onClick={handleClick} className='flashcard'>
+        {/* Back of the card */}
+        <p>Definition: {data.definition}</p>
+        <p>Explanation: {data.explanation}</p>
+        <p>Example: {data.example}</p>
+        <p>Tags: {data.tags}</p>
+      </div>
+    </ReactCardFlip>
+  );
+}
+
+
 const InputListener = () => {
-  const [word, setWord] = useState([]);
+  // Initialize state with the value in localStorage (if it exists) or an empty array
+  const [word, setWord] = useState(() => {
+    const savedWords = localStorage.getItem("words");
+    return savedWords ? JSON.parse(savedWords) : [];
+  });
 
   useEffect(() => {
     const socket = io('http://localhost:5000');
 
     socket.on('new word', (input_word, input_definition, input_explanation, input_example, input_tag) => {
-      setWord(prevWords => [...prevWords, {
-        word: input_word,
-        definition: input_definition,
-        explanation: input_explanation,
-        example: input_example,
-        tag: input_tag
-        //didn't implement parsing tags yet
-      }]);
+      setWord(prevWords => {
+        const newWords = [...prevWords, {
+          word: input_word,
+          definition: input_definition,
+          explanation: input_explanation,
+          example: input_example,
+          tag: input_tag
+          //didn't implement parsing tags yet
+        }];
+
+        // Save the new words array to localStorage
+        localStorage.setItem("words", JSON.stringify(newWords));
+
+        return newWords;
+      });
     });
 
     return () => {
@@ -81,6 +119,70 @@ const InputListener = () => {
     </div>
   );
 };
+
+
+const DICTIONARY = [
+  { word: 'apple', definition: 'A round fruit with red or yellow or green skin.' },
+  { word: 'banana', definition: 'Long curved fruit which grows in clusters and has soft pulpy flesh and yellow skin when ripe.' },
+  { word: 'cat', definition: 'A small domesticated carnivorous mammal with soft fur, a short snout, and retractile claws.' },
+  // You can add as many words and definitions as you want.
+];
+
+function Game() {
+  const [currentWord, setCurrentWord] = useState({});
+  const [score, setScore] = useState(0);
+  const [inputWord, setInputWord] = useState('');
+
+  // Initialize game with random word
+  React.useEffect(() => {
+    chooseRandomWord();
+  }, []);
+
+  function chooseRandomWord() {
+    const randomWord = DICTIONARY[Math.floor(Math.random() * DICTIONARY.length)];
+    setCurrentWord(randomWord);
+  }
+
+  function handleInputChange(event) {
+    setInputWord(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (inputWord.toLowerCase() === currentWord.word.toLowerCase()) {
+      setScore(score + 1);
+    }
+    chooseRandomWord();
+    setInputWord('');
+  }
+
+  return (
+    <div>
+      <h1>Word Game</h1>
+      <h2>Score: {score}</h2>
+      <p>Definition: {currentWord.definition}</p>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputWord}
+          onChange={handleInputChange}
+          placeholder="Type the word here"
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+
+
+
+}
+
+
+
+
+
 
 
 export default App;
