93932310aec60f4da30dc1d7dbe632770d8a0ebe
Harim Hahn <hmh79@cornell.edu>
2023-06-19 15:45:22 -0400

    can add multiple words

--- website-react/src/App.js
+++ website-react/src/App.js
@@ -16,54 +16,56 @@ function App() {
           This is my body
         </p>
         <WordList></WordList>
-        <MyComponent></MyComponent>
       </body>
     </div >
   );
 }
 
 function WordList() {
-
+  const word_example = {
+    word: 'example word',
+    definition: 'ex def',
+    explanation: 'ex exp',
+    example: 'ex ex',
+    tag: 'ex tag'
+  }
 
   return (
     <div>
       <h2>My word list: </h2>
 
-      <Word word="worddd" definition="deffff" explanation='expppp' example='exxxxxx' tags='tagggs'></Word>
-
+      <Word data={word_example} ></Word>
+      <InputListener></InputListener>
     </div>
   );
 }
 
-function Word({ word = "", definition = "", explanation = "", example = "", tags = "" }) {
+function Word({ data }) {
   return (
     <div>
-      <h2>{word}</h2>
-      <p>Definition: {definition}</p>
-      <p>Explanation: {explanation}</p>
-      <p>Example: {example}</p>
-      <p>Tags: {tags}</p>
+      <h2>{data.word}</h2>
+      <p>Definition: {data.definition}</p>
+      <p>Explanation: {data.explanation}</p>
+      <p>Example: {data.example}</p>
+      <p>Tags: {data.tags}</p>
     </div>
   );
 }
-const MyComponent = () => {
-  const [in_word, setWord] = useState('word not set');
-  const [in_def, setDef] = useState('');
-  const [in_exp, setExp] = useState('');
-  const [in_ex, setEx] = useState('');
-  const [in_tag, setTag] = useState('[tags not set yet, fix later]');
+const InputListener = () => {
+  const [word, setWord] = useState([]);
 
   useEffect(() => {
     const socket = io('http://localhost:5000');
 
     socket.on('new word', (input_word, input_definition, input_explanation, input_example, input_tag) => {
-      // on receiving a message, update states
-
-      setWord(input_word);
-      setDef(input_definition);
-      setExp(input_explanation);
-      setEx(input_example);
-      //setTag(input_tag);
+      setWord(prevWords => [...prevWords, {
+        word: input_word,
+        definition: input_definition,
+        explanation: input_explanation,
+        example: input_example,
+        tag: input_tag
+        //didn't implement parsing tags yet
+      }]);
     });
 
     return () => {
@@ -73,8 +75,9 @@ const MyComponent = () => {
 
   return (
     <div>
-
-      <Word word={in_word} definition={in_def} explanation={in_exp} example={in_ex} tags={in_tag}></Word>
+      {word.map((word, index) => (
+        <Word key={index} data={word} />
+      ))}
     </div>
   );
 };
