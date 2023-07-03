eeb4c84de914c4b8f9fe06e53c54b97da5887aa3
Harim Hahn <hmh79@cornell.edu>
2023-07-03 16:49:15 -0400

    multiple choice game

--- website-react/src/GamePage.js
+++ website-react/src/GamePage.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import io from 'socket.io-client';
+import ReactDOM from "react-dom";
 
 const GamePage = ({ words }) => {
+
   return <div>
     Start GamesPage
-    <Game words={words} />
+    <TypeWord words={words} />
+    <MultipleChoice words={words} />
     End GamesPage
   </div>;
 };
 
-function Game({ words }) {
+function TypeWord({ words }) {
   const [currentWord, setCurrentWord] = useState({});
   const [score, setScore] = useState(0);
   const [inputWord, setInputWord] = useState('');
@@ -29,11 +32,6 @@ function Game({ words }) {
       console.log('No words with definitions found.');
     }
   }
-
-
-
-
-
   function handleInputChange(event) {
     setInputWord(event.target.value);
   }
@@ -49,7 +47,7 @@ function Game({ words }) {
 
   return (
     <div>
-      <h1>Word Game</h1>
+      <h1>Type the Word Game</h1>
       <h2>Score: {score}</h2>
       <p>Definition: {currentWord.definition}</p>
 
@@ -65,10 +63,79 @@ function Game({ words }) {
       </form>
     </div>
   );
-
-
-
 }
+
+
+const MultipleChoice = ({ words }) => {
+  const [selectedWord, setSelectedWord] = useState(null);
+  const [options, setOptions] = useState([]);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    resetGame();
+  }, []);
+
+  const resetGame = () => {
+    // Remove applied classes from choices
+    options.forEach((option, index) => {
+      const element = document.getElementById(`option-${index}`);
+      element.classList.remove('correct');
+      element.classList.remove('wrong');
+    });
+
+    // Randomly select a word
+    const word = words[Math.floor(Math.random() * words.length)];
+    setSelectedWord(word);
+
+    // Get 4 other random words for options
+    const otherWords = words.filter(w => w !== word);
+    const randomOptions = shuffleArray(otherWords).slice(0, 3);
+    randomOptions.push(word);
+
+    // Shuffle the options
+    setOptions(shuffleArray(randomOptions));
+  };
+  const shuffleArray = (array) => {
+    const arr = array.slice();
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  };
+
+  const handleOptionClick = (option, index) => {
+    if (option === selectedWord) {
+      // Correct answer
+      document.getElementById(`option-${index}`).classList.add('correct');
+      setScore(score + 1);
+      setTimeout(resetGame, 500); // A small delay before resetting
+    } else {
+      // Wrong answer
+      document.getElementById(`option-${index}`).classList.add('wrong');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Multiple Choice Game</h1>
+      <h2>Word: {selectedWord && selectedWord.word}</h2>
+      <h3>Score: {score}</h3>
+      <p>Select the correct definition:</p>
+      <div className="choices-container">
+        {options.map((option, index) => (
+          <div key={index} id={`option-${index}`} className="choice" onClick={() => handleOptionClick(option, index)}>
+            {option.definition}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+
+
 
 
 export default GamePage;
