23d2a818c91447e92be8a9acb3323e1f445f7547
Harim Hahn <hmh79@cornell.edu>
2023-07-07 23:55:23 -0400

    css navbar + home

--- website-react/src/HomePage.js
+++ website-react/src/HomePage.js
@@ -1,20 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import './App.css';
-import io from 'socket.io-client';
-import './App.js';
+import { HashLink as Link } from 'react-router-hash-link';
 
 const HomePage = () => {
   return <div>
     Start HomePage
-    <Welcome />
+    <div className='welcome-wrapper'>
+      <Welcome />
+
+    </div>
+    <div className="arrow"></div>
     End HomePage
   </div>;
 };
 
+
+
 function Welcome() {
   return (
     <div className='welcome'>
-      Welcome!
+      <h1>Welcome</h1>
+      <p>Let's start studying!</p>
+
     </div>
   )
 }
