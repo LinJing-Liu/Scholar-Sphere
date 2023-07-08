e6db48cfdd9f1f56c059c1b2c3ac71b13561e155
Harim Hahn <hmh79@cornell.edu>
2023-07-08 10:56:15 -0400

    rough crossword

--- website-react/src/GamePage.js
+++ website-react/src/GamePage.js
@@ -1,14 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 import io from 'socket.io-client';
 import ReactDOM from "react-dom";
+import clg from 'crossword-layout-generator';
 
 const GamePage = ({ words }) => {
-
+  const testjson = [
+    {
+      "word": "abcd",
+      "clue": "first 4 letters of the alphabet"
+    },
+    {
+      "word": "efgh",
+      "clue": "next 4 letters of the alphabet"
+    },
+    // more word objects...
+  ]
+  //<CrosswordComponent words={testjson} ></CrosswordComponent>
   return <div>
     Start GamesPage
     <TypeWord words={words} />
     <MultipleChoice words={words} />
+
+    <CrosswordSkeleton words={words} ></CrosswordSkeleton>
+
     End GamesPage
   </div>;
 };
@@ -66,6 +81,98 @@ function TypeWord({ words }) {
 }
 
 
+
+
+const generateCrosswordLayoutSkeleton = (words) => {
+  const randomWords = words.words.slice().sort(() => 0.5 - Math.random());
+  const selectedWords = randomWords.slice(0, 2);
+
+  const input_json = selectedWords.map(word => ({ answer: word.word, clue: word.definition }));
+  const layout = clg.generateLayout(input_json);
+  const table = layout.table;
+
+  return {
+    table,
+    clues: selectedWords.map(word => word.definition),
+  };
+};
+
+const CrosswordSkeleton = ({ words }) => {
+  const [crosswordTable, setCrosswordTable] = useState([]);
+  const [clues, setClues] = useState([]);
+  const [userInput, setUserInput] = useState([]);
+  const [winner, setWinner] = useState(false);
+  const inputRefs = useRef([]);
+
+  useEffect(() => {
+    const { table, clues } = generateCrosswordLayoutSkeleton({ words });
+    setCrosswordTable(table);
+    setClues(clues);
+    const initialInput = table.map(row => row.map(cell => ''));
+    setUserInput(initialInput);
+  }, []);
+
+  const handleInputChange = (rowIndex, colIndex, e) => {
+    const newInput = userInput.map((row, i) =>
+      i !== rowIndex ? row : row.map((cell, j) =>
+        j !== colIndex ? cell : e.target.value.toUpperCase()
+      )
+    );
+    setUserInput(newInput);
+    if (newInput.every((row, i) =>
+      row.every((cell, j) => cell === crosswordTable[i][j])
+    )) {
+      setWinner(true);
+    }
+    moveToNextInput(rowIndex, colIndex);
+  };
+
+  const moveToNextInput = (rowIndex, colIndex) => {
+    // check if there's a valid next cell in the current row
+    if (colIndex + 1 < crosswordTable[rowIndex].length && crosswordTable[rowIndex][colIndex + 1] !== '-') {
+      inputRefs.current[rowIndex][colIndex + 1].focus();
+    } else {
+      // if not, go to the cell directly below
+      if (rowIndex + 1 < crosswordTable.length && crosswordTable[rowIndex + 1][colIndex] !== '-') {
+        inputRefs.current[rowIndex + 1][colIndex].focus();
+      }
+    }
+  };
+
+  return (
+    <div>
+      {crosswordTable.map((row, rowIndex) => (
+        <div key={rowIndex}>
+          {row.map((cell, colIndex) => {
+            const cellClass = cell === '-' ? 'cell-black' : 'cell-white';
+            return (
+              <input
+                key={colIndex}
+                className={`cell ${cellClass}`}
+                readOnly={cell === '-'}
+                disabled={cell === '-'}
+                value={userInput[rowIndex][colIndex] || ''}
+                maxLength={1}
+                onChange={(e) => handleInputChange(rowIndex, colIndex, e)}
+                ref={(el) => {
+                  if (!inputRefs.current[rowIndex]) {
+                    inputRefs.current[rowIndex] = [];
+                  }
+                  inputRefs.current[rowIndex][colIndex] = el;
+                }}
+              />
+            );
+          })}
+        </div>
+      ))}
+      {winner && <h2>Winner!</h2>}
+      <h3>Clues:</h3>
+      <ul>
+        {clues.map((clue, index) => <li key={index}>{clue}</li>)}
+      </ul>
+    </div>
+  );
+};
 const MultipleChoice = ({ words }) => {
   const [selectedWord, setSelectedWord] = useState(null);
   const [options, setOptions] = useState([]);
