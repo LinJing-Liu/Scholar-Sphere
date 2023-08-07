3771bf2676c17ad39631b7f9519bec8071fffea8
LinJing-Liu <linjingl93@gmail.com>
2023-08-07 00:04:30 -0400

    update flashcard page
    - fix inconsistency in filter dropdown initially and when selecting all options
    - add display filters to flashcards
    - update css
    - add flashcard page instruction button

--- website-react/src/js/FilterDropdown.js
+++ website-react/src/js/FilterDropdown.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const FilterDropdown = ({ display, label, options, selection, onSelect }) => {
+const FilterDropdown = ({ display, label, options, selection, onSelect, displaySelection = true }) => {
     const selectOption = (event) => {
         let newSelection = [];
         let selected = event.target.value;
@@ -11,6 +11,10 @@ const FilterDropdown = ({ display, label, options, selection, onSelect }) => {
                 [...document.querySelectorAll("." + label + "-check-input")].map(input => input.checked = true);
             } else {
                 newSelection = selection.concat(selected);
+
+                if(newSelection.length == options.length) {
+                    [...document.querySelectorAll("." + label + "-check-input")].map(input => input.checked = true);
+                }
             }
         } else {
             // remove option
@@ -32,18 +36,20 @@ const FilterDropdown = ({ display, label, options, selection, onSelect }) => {
                 </button>
                 <div class="dropdown-menu" aria-labelledby={label + "dropdownBtn"}>
                     <div class="form-check selectAll">
-                        <input class={"form-check-input" + " " + label + "-check-input"} type="checkbox" value="all" id={"flexCheckAll" + label} onChange={selectOption} />
+                        <input class={"form-check-input" + " " + label + "-check-input"} type="checkbox" value="all" id={"flexCheckAll" + label} onChange={selectOption}
+                            defaultChecked={options.length == selection.length}/>
                         <label class={"form-check-label" + " " + label + "-check-label"} for={"flexCheckAll" + label}>Select All</label>
                     </div>
                     {options.map((option, id) => 
                     <div class="form-check" key={id}>
-                        <input class={"form-check-input" + " " + label + "-check-input"} type="checkbox" value={option} id={"flexCheck" + option} onChange={selectOption}/>
+                        <input class={"form-check-input" + " " + label + "-check-input"} type="checkbox" value={option} id={"flexCheck" + option} onChange={selectOption} 
+                            defaultChecked={selection.filter(t => t == option).length > 0} />
                         <label class={"form-check-label" + " " + label + "-check-label"} for={"flexCheck" + option}>{option}</label>
                     </div>
                     )}
                 </div>
             </div>
-            {selection.map((option, id) => <span class="badge" key={id}>{option}</span>)}
+            {displaySelection && selection.map((option, id) => <span class={"badge " + label + "Badge"} key={id}>{option}</span>)}
         </div>
     );
 }
--- website-react/src/js/FlashCardPage.js
+++ website-react/src/js/FlashCardPage.js
@@ -2,39 +2,63 @@ import React, { useEffect, useState } from 'react';
 import FilterDropdown from './FilterDropdown';
 
 import '../css/FlashCardPage.css';
+import helpIcon from '../img/help.png';
+
+const allConfidence = ["1", "2", "3", "4", "5"];
+const contentDisplay = ["Word", "Definition", "Explanation", "Example"];
 
 const FlashCardPage = ({ words, tags }) => {
-  const [tagSelected, setTagSelected] = useState([]);
-  const [confidenceSelected, setConfidenceSelected] = useState([]);
+  const [tagSelected, setTagSelected] = useState(tags);
+  const [confidenceSelected, setConfidenceSelected] = useState(allConfidence);
 
   const filteredWords = words.filter(word =>
-    (confidenceSelected.includes(word.confidence) || confidenceSelected.length == 0)
-    && (word.tag.filter(t => tagSelected.includes(t)).length > 0 || tagSelected.length == 0)
+    confidenceSelected.includes(word.confidence)
+    && (word.tag.filter(t => tagSelected.includes(t)).length > 0
+    || (tagSelected.length == tags.length && word.tag.length == 0))
   );
+
+  const [frontSelected, setFrontSelected] = useState(["Word"]);
+  const [backSelected, setBackSelected] = useState(["Definition"]);
+
   return <div id="flash-card-page-container">
     <br></br>
-    <h1 id="flashcardsHeading">Flashcards</h1>
-    <div id="filterContainer">
-      <div id="filterHeading">Filters</div>
+    <h1 id="flashcardsHeading">
+      Flashcards
+      <img src={helpIcon} data-toggle="modal" data-target="#helpModalCenter" id="cardHelpBtn"/>
+    </h1>
+    <FlashcardHelpModal />
+
+    <div id="displayFilterContainer">
+      <div id="displayFilterHeading">Display Filters</div>
       <div class="row">
         <div class="col">
-          <FilterDropdown display="Tags" label="tags" options={tags} selection={tagSelected} onSelect={setTagSelected} />
+          <FilterDropdown display="Front Display" label="frontDisplay" options={contentDisplay} selection={frontSelected} onSelect={setFrontSelected} displaySelection={false} />
         </div>
         <div class="col">
-          <FilterDropdown display="Confidence Level" label="confidence-level" options={["1", "2", "3", "4", "5"]} selection={confidenceSelected} onSelect={setConfidenceSelected} />
+          <FilterDropdown display="Back Display" label="backDisplay" options={contentDisplay} selection={backSelected} onSelect={setBackSelected} displaySelection={false} />
         </div>
       </div>
     </div>
-    <FlashCards words={filteredWords}></FlashCards>
+    <br />
+    <div id="filterContainer">
+      <div id="cardFilterHeading">Filters</div>
+      <div class="row">
+        <div class="col">
+          <FilterDropdown display="Tags" label="cardTags" options={tags} selection={tagSelected} onSelect={setTagSelected}/>
+        </div>
+        <div class="col">
+          <FilterDropdown display="Confidence Level" label="cardConfidenceLevel" options={allConfidence} selection={confidenceSelected} onSelect={setConfidenceSelected} />
+        </div>
+      </div>
+    </div>
+    <FlashCards words={filteredWords} front={frontSelected} back={backSelected} />
   </div>;
 };
 
-
-function FlashCards({ words }) {
+function FlashCards({ words, front, back }) {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const isEmpty = (words.length === 0);
 
-  const isEmpty = (words.length === 0)
-  console.log(words, currentIndex, isEmpty)
   useEffect(() => {
     const handleKeyDown = (e) => {
       // This line prevents the default action (scrolling) from occurring
@@ -63,30 +87,26 @@ function FlashCards({ words }) {
 
   return (
     <div>
-      {currentCard && !isEmpty ? 
+      {!isEmpty ? 
         <div>
-          <FlashCard data={currentCard} />
-          <p>
+          <FlashCard data={currentCard} front={front} back={back} />
+          <p className="cardCountLabel">
             {currentIndex + 1} out of {words.length}
           </p>
         </div>
-      : (isEmpty ?
-        <div>
-          <p>
-            No words match your filters.
-          </p>
-        </div> 
-        : <div id="addWordNotice">
-          Add word to start using flashcards.
+      : <div id="addWordNotice">
+          There is no word in the inventory or the filters have filtered out all words.
           <br />
           <a onClick={() => window.location.href="/word-list"}>Go to word list page to add word.</a>
-        </div>)
+        </div>
       }
     </div>
   );
 }
-const FlashCard = ({ data }) => {
+
+const FlashCard = ({ data, front, back }) => {
   const [isFlipped, setIsFlipped] = useState(false);
+  
   useEffect(() => {
     const handleKeyDown = (e) => {
       if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
@@ -98,30 +118,76 @@ const FlashCard = ({ data }) => {
 
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, []);
+
   const toggleFlip = () => {
     setIsFlipped(!isFlipped);
   };
+
+  function getDisplay(displayArr) {
+    // current content type - Word, Definition, Explanation, Example
+    var display = [];
+    for(var displayType of displayArr) {
+      if(displayType == "Word") {
+        display.push(data.word);
+      } else if(displayType == "Definition") {
+        display.push("Definition: " + data.definition);
+      } else if (displayType == "Explanation") {
+        display.push("Explanation: " + data.explanation);
+      } else if(displayType == "Example") {
+        display.push("Example: " + data.example);
+      }
+    }
+
+    return display;
+  }
 
   return (
     <div className="flashcard-container">
       <div className={`flashcard ${isFlipped ? 'flashcard-flipped' : ''}`} onClick={toggleFlip}>
         <div className="flashcard-side flashcard-front">
           {/* Front of the card */}
-          <h2>{data.word}</h2>
+          <div className="flashcard-content">
+            {getDisplay(front).map((str, id) => <div className="flashcard-display-item" key={id}>{str}</div>)}
+          </div>
         </div>
         <div className="flashcard-side flashcard-back">
           {/* Back of the card */}
           <div className="flashcard-content">
-            <ul>
-              <li>Definition: {data.definition}</li>
-              <li>Explanation: {data.explanation}</li>
-              <li>Example: {data.example}</li>
-              <li>Tags: {data.tags}</li>
-            </ul>
+            {getDisplay(back).map((str, id) => <div className="flashcard-display-item" key={id}>{str}<br /></div>)}
           </div>
         </div>
       </div>
     </div>
   );
 };
+
+const FlashcardHelpModal = () => {
+  return (
+    <div class="modal fade" id="helpModalCenter" tabindex="-1" role="dialog" aria-labelledby="FlashcardInstructions" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="cardHelpModalTitle">Flashcard Instructions</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <ul class="flashcardModalList">
+              <li>Press up or down arrow to switch between cards in order.</li>
+              <li>Press left or right arrow to flip the card.</li>
+              <li>Can also click on the card to flip it.</li>
+              <li>Use the display filters to customize what is displayed on the front and back sides of the card.</li>
+              <li>Use the regular filters to customize which groups of cards to view.</li>
+            </ul>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary modalCloseBtn" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default FlashCardPage;
